82c671ea8b229538a198267733fdcaa7ece70fa6
R. LaRoi <are@raewhoo.com>
2024-08-15 14:13:38 -0400

    Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -41,7 +41,26 @@ export function Home({ products }: HomeProps) {
         )}
       </div>
       <div className="mt-20">
-        {/* <ArtistProfiles profiles={profiles} /> */}
+        <section className="ap-container">
+          <div className="text-center text-lg">
+            <h1 className=" ap-title text-8xl  text-center text-gray-900  ">ARTIST PROFILE</h1>
+            <p className="text-xl pb-4">Follow our monthly selections and be a part of a vibrant <br /> community of music lovers who share your passion for discovering new sounds.</p>
+            <div className="ap-line"></div>
+          </div>
+          <div className="artist-profile mt-10">
+            <div className="column-one">
+              <img src="https://github.com/user-attachments/assets/98f3d686-b656-4556-8c6c-392a1a4f46b8" alt="" className="image-profile" />
+            </div>
+            <div className="column-two">
+              <div>
+                <h1 className="text-7xl">Shakes & Les</h1>
+                <h3>FUNK 99</h3>
+                <p className="ap-desc">"Funk 99" is more than just a song; it's a celebration of culture, rhythm, and community. With its catchy intro and engaging lyrics, Shakes & Les, along with LeeMcKrazy, have crafted a track that not only entertains but also invites listeners to join in the fun. As the Amapiano genre continues to evolve, "Funk 99" stands out as a testament to the creativity and vibrancy of South African music.</p>
+
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
 
       <Footer />
